04910afcf76b5d2ff6cf10b378a266e3484b5770
Gohar Irfan Chaudhry <girfan@mit.edu>
2024-06-08 12:34:32 +0000

    Distributed inference works with TP=2 on sr01 and sr02

--- examples/offline_inference.py
+++ examples/offline_inference.py
@@ -1,4 +1,5 @@
 from vllm import LLM, SamplingParams
+import torch
 
 # Sample prompts.
 prompts = [
@@ -8,7 +9,19 @@ prompts = [
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 # Create an LLM.
-llm = LLM(model="facebook/opt-125m", disable_log_stats=False)
+# llm = LLM(model="facebook/opt-125m", disable_log_stats=False, tensor_parallel_size=2)
+
+llm = LLM(
+    model="TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ",
+    quantization="gptq",
+    dtype=torch.float16,
+    tensor_parallel_size=2,
+    max_model_len=16384,
+    revision="gptq-4bit-32g-actorder_True",
+    gpu_memory_utilization=0.75,
+    disable_custom_all_reduce=True,
+    enforce_eager=True)
+
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 outputs = llm.generate(prompts, sampling_params)
